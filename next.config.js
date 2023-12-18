4df7887b5f7cc5a6c198ed08abd77ebe533f8adb
Oussama Louati <louati.oussama@ine.inpt.ma>
2023-12-18 11:38:34 +0100

    trying to solve problems related to server actions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    serverActions: {
+      // Enable server action logging
+      enableLogging: true,
+      },
     serverComponentsExternalPackages: ["mongoose"],
   },
   eslint: {
