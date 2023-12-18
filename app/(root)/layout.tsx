4f269aee3ce1d90cf5e597b6ee2db2655a8477dc
Oussama Louati <louati.oussama@ine.inpt.ma>
2023-12-18 18:31:24 +0100

    Update font import and layout structure

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import Head from "next/head"; // Import Head to set the font
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 
@@ -9,8 +9,6 @@ import LeftSidebar from "@/components/shared/LeftSidebar";
 import Bottombar from "@/components/shared/Bottombar";
 import RightSidebar from "@/components/shared/RightSidebar";
 import Topbar from "@/components/shared/Topbar";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Threads",
@@ -28,14 +26,20 @@ export default function RootLayout({
         baseTheme: dark,
       }}
     >
-      <html lang='en'>
-        <body className={inter.className}>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
+        />
+      </Head>
+      <html lang="en">
+        <body className="font-roboto">
           <Topbar />
 
-          <main className='flex flex-row'>
+          <main className="flex flex-row">
             <LeftSidebar />
-            <section className='main-container'>
-              <div className='w-full max-w-4xl'>{children}</div>
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
             </section>
             {/* @ts-ignore */}
             <RightSidebar />
